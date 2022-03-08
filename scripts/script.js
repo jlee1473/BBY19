205886159eab4d79fcf107d8f8d1607add2b6622
Gabriel Fairbairn A00888969 / NerdSlayer0 <gabrielfair@hotmail.com>
2022-03-07 22:41:50 -0800

    first attempt at callback for firestore data on invite.html

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,17 @@ function sayHello() {
     //do something
 }
 //sayHello();    //invoke function
+
+function read_display_Team() {
+    //name of the collection and documents should matach excatly with what you have in Firestore
+    db.collection("team").doc("lv9OWXT8E8UFPr5ypBFF") 
+        .onSnapshot(teamDoc => { //arrow notation
+            console.log("current team name: " + teamName.data()); //.data() returns data object
+            //using javascript to display the data on the right place
+            document.getElementById("teamName").innerHTML = lv9OWXT8E8UFPr5ypBFF.data().teamName;
+
+            //Here are other ways to access key:value data fields
+            //$('#quote-goes-here').text(c.data().quote);        //using jquery object dot notation
+            //$("#quote-goes-here").text(c.data()["quote"]);      //using json object indexing
+        })
+}
