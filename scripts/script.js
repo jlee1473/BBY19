678587f82a7b052af965f3805196e45d0f1f3f9b
Gabriel Fairbairn A00888969 / NerdSlayer0 <gabrielfair@hotmail.com>
2022-03-08 00:13:39 -0800

    Added some .js stuff that may or may not work. Added buttons to connect page, and separated invite.html, createteam.html, and jointhisteam.html

--- scripts/script.js
+++ scripts/script.js
@@ -8,16 +8,54 @@ function sayHello() {
 }
 //sayHello();    //invoke function
 
-function read_display_Team() {
-    //name of the collection and documents should matach excatly with what you have in Firestore
-    db.collection("team").doc("lv9OWXT8E8UFPr5ypBFF") 
-        .onSnapshot(teamDoc => { //arrow notation
-            console.log("current team name: " + teamName.data()); //.data() returns data object
-            //using javascript to display the data on the right place
-            document.getElementById("teamName").innerHTML = lv9OWXT8E8UFPr5ypBFF.data().teamName;
 
-            //Here are other ways to access key:value data fields
-            //$('#quote-goes-here').text(c.data().quote);        //using jquery object dot notation
-            //$("#quote-goes-here").text(c.data()["quote"]);      //using json object indexing
-        })
+function insertTeam() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {                                                                 
+            // Do something for the current logged-in user here: 
+            console.log(user.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("team").doc(user.uid);
+            //get the document for current user.
+            currentUser.get()
+                  .then(userDoc => {
+               var current_Teams = userDoc.data().teamName;
+               console.log(current_Teams);
+               //method #1:  insert with html only
+               document.getElementById("team-name-here").innerText = current_Teams;    //using javascript
+               //method #2:  insert using jquery
+            //    $("#team-name-here").text(current_Teams);                         //using jquery
+            })
+        } else {
+            console.log("Please sign in");
+        }
+    });
 }
+insertTeam();
+
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {                                                                 
+            // Do something for the current logged-in user here: 
+            console.log(user.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for current user.
+            currentUser.get()
+                  .then(userDoc => {
+               var user_Name = userDoc.data().name;
+               console.log(user_Name);
+               //method #1:  insert with html only
+               document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+               //method #2:  insert using jquery
+               $("#name-goes-here").text(user_Name);                         //using jquery
+            })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+insertName();
+
