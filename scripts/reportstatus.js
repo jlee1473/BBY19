cea33d880ea86e5199a909eee6005ea778a91dce
Jason Lee <jlee1473@my.bcit.ca>
2022-03-08 18:50:30 -0800

    added reportstatus.js -Jason

---
+++ scripts/reportstatus.js
@@ -0,0 +1,29 @@
+function submitReport() {
+  console.log("in")
+  let Healthy = document.querySelector('input[name="status"]:checked').value;
+  let Recovering = document.querySelector('input[name="status"]:checked').value;
+  console.log(Healthy, Recovering);
+
+  firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+          var currentUser = db.collection("users").doc(user.uid)
+          var userID = user.uid;
+          //get the document for current user.
+          currentUser.get()
+              .then(userDoc => {
+                  var userEmail = userDoc.data().email;
+                  db.collection("Reports").add({
+                      healthy: Healthy,
+                      recovering: Recovering,
+                      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                  }).then(()=>{
+                      window.location.href = "report4.html"; //new line added
+                  })
+              })
+                 
+      } else {
+          // No user is signed in.
+      }
+  });
+
+}
