bf0921a7a86cb63b8ae91614ea14ecb292710c6c
Jason Lee <jlee1473@my.bcit.ca>
2022-03-08 19:34:07 -0800

    Modified reportstatus.js to add data to Reports collection -Jason

--- scripts/reportstatus.js
+++ scripts/reportstatus.js
@@ -1,29 +1,28 @@
 function submitReport() {
-  console.log("in")
-  let Healthy = document.querySelector('input[name="status"]:checked').value;
-  let Recovering = document.querySelector('input[name="status"]:checked').value;
-  console.log(Healthy, Recovering);
+    console.log("in")
+    let Healthy = document.querySelector('input[name="status"]:checked').value;
+    console.log(Healthy);
 
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          var currentUser = db.collection("users").doc(user.uid)
-          var userID = user.uid;
-          //get the document for current user.
-          currentUser.get()
-              .then(userDoc => {
-                  var userEmail = userDoc.data().email;
-                  db.collection("Reports").add({
-                      healthy: Healthy,
-                      recovering: Recovering,
-                      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                  }).then(()=>{
-                      window.location.href = "report4.html"; //new line added
-                  })
-              })
-                 
-      } else {
-          // No user is signed in.
-      }
-  });
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    var userEmail = userDoc.data().email;
+                    db.collection("Reports").add({
+                        userID: userID,
+                        currentStatus: Healthy,
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                    }).then(()=>{
+                        window.location.href = "report4.html"; //new line added
+                    })
+                })
+                   
+        } else {
+            // No user is signed in.
+        }
+    });
 
 }
