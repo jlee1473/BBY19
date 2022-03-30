8f066631828c6904e3416e926bac01b77013ea72
Andrew Chu <andrewchu06@gmail.com>
2022-03-29 20:10:32 -0700

    garbage collection - Andrew

--- scripts/records.js
+++ scripts/records.js
@@ -8,14 +8,8 @@ function populateStatusDynamically() {
         .then(allusers => {
             allusers.forEach(doc => {
                 var teamMember = doc.data().name; //gets the name field
-                // var hikeID = doc.data().id; //gets the unique ID field
-                // var hikeLength = doc.data().length; //gets the length field
                 let testRecordsTable = recordsTableTemplate.content.cloneNode(true);
                 testRecordsTable.querySelector('.user-name').innerHTML = teamMember;
-                // testHikeCard.querySelector('.card-length').innerHTML = hikeLength;
-                // testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);
-                // testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;
-                // var status = getStatus(doc.id); 
                 var status = doc.data().latestStatus;
                 testRecordsTable.querySelector('.health-status').innerHTML = status;
                 var lastUpdate = doc.data().latestStatusTimeStamp.toDate();
