c8beecfa49fa2fb421e1613b943259286f289deb
Andrew Chu <andrewchu06@gmail.com>
2022-04-02 11:38:55 -0700

    Cleaned up some code-Andrew

--- scripts/records.js
+++ scripts/records.js
@@ -38,9 +38,8 @@ function displayMyTeam() {
         }
     })
 }
-// displayMyTeam();
 
-//goes to the users collection, and gives everyone "ok", or "sick"
+//goes to the users collection, and gives everyone "healthy", or "recovering"
 function updateHealth(status) {
     //define a variable for the collection you want to create in Firestore to populate data
     db.collection("users")
@@ -65,76 +64,25 @@ function listenStatus() {
 }
 listenStatus();
 
-function displayEmployees() {
-    db.collection("users")
-         .get()
-         .then(function (snap) {
-             document.getElementById("status-goes-here").innerHTML = ""
-             snap.forEach(doc => {
-                 //console.log(doc.data());
-                 //console.log(doc.data().name);
-                 console.log(doc.data().latestStatus);
-                 if (doc.data().latestStatus == "recovering")
-                 showStatus();
-                    //  document.getElementById("status-goes-here").innerHTML +=
-                    //  "<button type='button' class='btn btn-danger'>" +
-                    //  doc.data().name + " is " + doc.data().latestStatus + "</button> <br>"
-                 else
-                     document.getElementById("status-goes-here").innerHTML +=
-                     "<button type='button' class='btn btn-success'>" +
-                     doc.data().name + " is " + doc.data().latestStatus + "</button> <br>"
-             })
-         })
-}
-
-        
-    // function displayEmployees() {
-    //     db.collection("users")
-    //          .get()
-    //          .then(function (snap) {
-    //              document.getElementById('.health-status').innerHTML = ""
-    //              snap.forEach(doc => {
-    //                  //console.log(doc.data());
-    //                  console.log(doc.data().name);
-    //                  console.log(doc.data().status);
-    //                  if (doc.data().latestStatus == "recovering")
-    //                      document.getElementById('.health-status').innerHTML +=
-    //                      "<button type='button' class='btn btn-danger'>" +
-    //                      doc.data().name + " is " + doc.data().latestStatus + "</button> <br>"
-    //                  else
-    //                      document.getElementById('.health-status').innerHTML +=
-    //                      "<button type='button' class='btn btn-success'>" +
-    //                      doc.data().name + " is " + doc.data().latestStatus + "</button> <br>"
-    //              })
-    //          })
-    //      }
-
-    
-// sick();
-
-// function sick() {
-//     if ("latestStatus", "==", "healthy"){
-//         var removeStatus = document.getElementById("colorID");
-//         removeStatus.classList.remove(badge bg-success);
-//         removeStatus.classList.add(badge bg-warning);
-//     } else {
-//         removeStatus.classList.remove(badge bg-warning);
-//         removeStatus.classList.add(badge bg-success);
-//     }
-//     }
-
-
-//This function will be reserved for pulling all records for a user after a teams link.
-// function getStatus(id) {
-//             db.collection("users").doc(id).get()
-//                 .then(doc => {
-//                     console.log(doc.data().currentStatus);
-//                     // var memberStatus = doc.data().currentStatus;
-//                     // let testRecordsTable = recordsTableTemplate.content.cloneNode(true);
-//                     // testRecordsTable.querySelector('.health-status').innerHTML = memberStatus;
-//                     // hikeCardGroup.appendChild(testRecordsTable);
-//                 })
-//         }
-
-
-    
+//Function initially created to insert correct button colour based on employee status. Not needed now but would like to keep.
+// function displayEmployees() {
+//     db.collection("users")
+//          .get()
+//          .then(function (snap) {
+//              document.getElementById("status-goes-here").innerHTML = ""
+//              snap.forEach(doc => {
+//                  //console.log(doc.data());
+//                  //console.log(doc.data().name);
+//                  console.log(doc.data().latestStatus);
+//                  if (doc.data().latestStatus == "recovering")
+//                  showStatus();
+//                     //  document.getElementById("status-goes-here").innerHTML +=
+//                     //  "<button type='button' class='btn btn-danger'>" +
+//                     //  doc.data().name + " is " + doc.data().latestStatus + "</button> <br>"
+//                  else
+//                      document.getElementById("status-goes-here").innerHTML +=
+//                      "<button type='button' class='btn btn-success'>" +
+//                      doc.data().name + " is " + doc.data().latestStatus + "</button> <br>"
+//              })
+//          })
+// }
