5293a9ff5d7364e38069742e283478f30adec4f6
Gabriel Fairbairn A00888969 / NerdSlayer0 <gabrielfair@hotmail.com>
2022-04-07 15:26:52 -0700

    formatted invite.js, teams.js and other minor fixes

--- scripts/invite.js
+++ scripts/invite.js
@@ -1,3 +1,5 @@
+
+// Displays the data from the memberOf field in the "team-id" box
 function displayMyTeam() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -5,7 +7,6 @@ function displayMyTeam() {
             db.collection("users").doc(userID).get().then(doc => {
                 team = doc.data().memberOf;
                 console.log(team);
-                
                 document.getElementById("team-id").innerHTML = team; 
             })
         }
--- scripts/teams.js
+++ scripts/teams.js
@@ -1,54 +1,60 @@
+
+// When called, the current user will create a new team in the db
+// and their userID will be added to that team, then redirect to invite page
 function createTeam() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             var userDoc = db.collection("users").doc(user.uid)
             var userID = user.uid;
-            //get the document for current user.
+            // Get the document for current user.
             userDoc.get()
-
             var teamID = db.collection("team");
             // Team successfully created.
-            // creates a new team with a unique ID and adds the user to that team
+            // Creates a new team with a unique ID and adds the user to that team
             teamID.add({
                 teamMembers: [userID]
             }).then(function (doc) {
                 console.log("New team added to firestore");
-                window.location.assign("join-team-complete.html"); //re-direct to invite.html after signup
-                updateTeam(doc.id, userID);
+                // Re-direct to invite.html after signup
+                window.location.assign("join-team-complete.html"); 
+                // Updates current users' memberOf field
+                updateTeam(doc.id, userID); 
             }).catch(function (error) {
                 console.log("Error adding new team: " + error);
             })
         } else {
-            console.log("Please log in"); // No user is signed in.
+            // If no user is signed in.
+            console.log("Please log in"); 
             window.location.href = "index.html";
         }
     })
 }
 
+// When called, the users ID will be added to the 
+// team matching the value in the jointeam textbox
 function joinTeam() {
-
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
         if (user) {
-            // Do something for the current logged-in user here: 
-            //teamID and userID are put into variables
+            // TeamID and userID are put into variables
             let userID = user.uid;
             var userDoc = db.collection("users").doc(user.uid)
-
             db.collection("team").get()
                 .then(
                     snap => {
                         snap.forEach(doc => {
                             var teamID = doc.id;
-                            var joinID = document.getElementById("jointeam").value;
+                            // Compares value in the jointeam textbox to each teamID in the database
+                            var joinID = document.getElementById("jointeam").value; 
                             joinID = joinID.trim();
-                            console.log(doc.id);
-                            console.log(joinID);
-                            if (joinID == teamID) {
+                            console.log(doc.id, joinID); 
+                            // If there is a match, the user is added to that team
+                            if (joinID == teamID) { 
                                 db.collection("team").doc(joinID).update({
                                     teamMembers: firebase.firestore.FieldValue.arrayUnion(userID)
                                 }).then(() => {
-                                    updateTeam(teamID, userID);
+                                    // Then updates the users' memberOf field
+                                    updateTeam(teamID, userID); 
                                 })
                             }
                         })
@@ -59,6 +65,8 @@ function joinTeam() {
     })
 }
 
+// MemberOf field of the user is replaced with the new teamID
+// Input parameters: The new team id of the requested user and their user id
 function updateTeam(teamID, userID) {
     db.collection("users").doc(userID).update({
         memberOf: teamID
