a6b6a08b89d320d7e9d151f8a07da7d2da76163a
Jason Lee <jlee1473@my.bcit.ca>
2022-04-07 17:07:39 -0700

    Commented homepage.js -Jason

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,38 +1,33 @@
 var currentUser;
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-        currentUser = db.collection("users").doc(user.uid);   //global
+        currentUser = db.collection("users").doc(user.uid); 
         console.log(currentUser);
 
         // the following functions are always called when someone is logged in
-        //read_display_Quote();
+
+        // Insert the users' name into homepage.html
         insertName();
+        // Insert the users' status into homepage.html
         insertStatus();
-        //populateCardsDynamically();
     } else {
         // No user is signed in.
         console.log("No user is signed in");
+        // Redirect to login page if no user is signed in.
         window.location.href = "login.html";
     }
 });
 
-// ------------------------------------------------------------------------
 // Called in homepage.html to display the current user's name onto the 
 // landing page.
-// ------------------------------------------------------------------------
 function insertName() {
-    // to check if the user is logged in:
-
 
     currentUser.get().then(userDoc => {
-        //get the user name
+        // get the name field
         var user_Name = userDoc.data().name;
         console.log(user_Name);
-        $("#name-goes-here").text(user_Name); //jquery
-        // document.getElementByID("name-goes-here").innetText=user_Name;
-
-
-
+        // insert the users' name into the welcome message
+        $("#name-goes-here").text(user_Name);
     })
 }
 
@@ -44,19 +39,24 @@ function insertStatus() {
     let greenStatus = document.getElementById("greenStatus");
     let redStatus = document.getElementById("redStatus");
     let statusData = document.getElementById("insertStatus");
+
+    // Clears the html stored in the statusData variable which is pointing to the "insertStatus" id.
     statusData.innerHTML = "";
 
     currentUser.get().then(userDoc => {
+        // gets the users' latest health status from the latestStatus field.
         var status = userDoc.data().latestStatus;
-        console.log(status);
         if (status == "recovering")
-            statusLabel = redStatus.content.cloneNode(true);
+            // Copies the attributes of the redStatus template.
+            statusLabel = redStatus.content.cloneNode(true); 
         else
-            statusLabel = greenStatus.content.cloneNode(true);
-        statusLabel.querySelector('.status-goes-here').innerHTML = status;
-        // console.log(status);
+            // Copies the attributes of the greenStatus template.
+            statusLabel = greenStatus.content.cloneNode(true); 
 
-        // $("#status-goes-here").text(status);
+        // Inserts the status into the appropriate template
+        statusLabel.querySelector('.status-goes-here').innerHTML = status;
+
+        // inserts this information into the corresponding redStatus or greenStatus template
         statusData.appendChild(statusLabel);
     })
 }
